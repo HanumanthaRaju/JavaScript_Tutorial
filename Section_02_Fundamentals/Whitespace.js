6a40200cd51685db207e9e07d3a3ddf2c34671c5
HanumanthaRaju <44515813+HanumanthaRaju@users.noreply.github.com>
2022-07-15 12:18:57 +0530

    Add files via upload

---
+++ Section_02_Fundamentals/Whitespace.js
@@ -0,0 +1,33 @@
+//Whitespace
+
+let formatted = true; if (formatted) {console.log('The code is easy to read');}
+
+//It’s is equivalent to the following code that uses whitespace. Hence, this code is much easy to read:
+
+let formatted1 = true;
+
+if (formatted1) {
+  console.log('The code is easy to read');
+}
+
+//Statement
+let message = "Welcome to JavaScript";
+console.log(message);
+
+//Blocks
+if (message=="Welcome") {
+  console.log('The local storage is supported');
+}
+
+/* Multi-line or Block level
+comments */
+
+let x=10;
+let y=20;
+
+//Expression
+x=x+y;
+console.log(x);
+
+
+

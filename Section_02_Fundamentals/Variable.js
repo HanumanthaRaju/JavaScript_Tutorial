d46f61d31a98e76e7a1490b54d3b787fcaaa42a4
HanumanthaRaju <44515813+HanumanthaRaju@users.noreply.github.com>
2022-07-15 14:42:52 +0530

    Add files via upload

---
+++ Section_02_Fundamentals/Variable.js
@@ -0,0 +1,16 @@
+//Declaring a variable
+let message;
+//Initialization of variable
+let message1="Hello";
+console.log(message1);
+message1="HelloWorld";
+console.log(message1);
+
+let x;
+console.log(x); //Undefined variable
+//console.log(y); //Undeclared variable
+
+//Constants
+const z=10;
+console.log(z);
+z=20;

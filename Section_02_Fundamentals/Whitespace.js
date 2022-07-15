e8e3051a5da7e25ee4d3d57ed17c0f49c2b32e41
HanumanthaRaju <44515813+HanumanthaRaju@users.noreply.github.com>
2022-07-15 11:20:52 +0530

    Delete Whitespace.js

--- Section_02_Fundamentals/Whitespace.js
+++
@@ -1,9 +0,0 @@
-let formatted = true; if (formatted) {console.log('The code is easy to read');}
-
-//It’s is equivalent to the following code that uses whitespace. Hence, this code is much easy to read:
-
-let formatted1 = true;
-
-if (formatted1) {
-  console.log('The code is easy to read');
-}

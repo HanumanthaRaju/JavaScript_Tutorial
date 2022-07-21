8096d343da6e5b31b98f82a985e063b9d79384de
HanumanthaRaju <44515813+HanumanthaRaju@users.noreply.github.com>
2022-07-21 12:39:29 +0530

    Add files via upload

---
+++ Section_03_Operators/Operator_with_Object.js
@@ -0,0 +1,20 @@
+let energy = {
+    valueOf() {
+      return 100;
+    },
+};
+  
+let currentEnergy = energy - 10;
+console.log(currentEnergy);
+  
+currentEnergy = energy + 100;
+console.log(currentEnergy);
+  
+currentEnergy = energy / 2;
+console.log(currentEnergy);
+  
+currentEnergy = energy * 1.5;
+console.log(currentEnergy);
+
+
+  

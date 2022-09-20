659af54bbab3f196c8ad6541ce8a00480f13a911
HanumanthaRaju <44515813+HanumanthaRaju@users.noreply.github.com>
2022-09-20 11:24:28 +0530

    Add files via upload

---
+++ Section_04_Control_Flow_Statements/ifNested.js
@@ -0,0 +1,19 @@
+let age=18;
+//Simple if statement
+if(age>=18) {
+    console.log("You can drive");
+}
+//Nested if Statement
+let age1=16;
+state="Karnataka";
+if(state="Karnataka") {
+    if(age1>=16) {
+        console.log("You can drive");
+    }
+}
+//Avoid Nested if and use logical operators
+if(state="Karnataka" && age1>=16) {
+    console.log("You can drive");
+}
+
+
